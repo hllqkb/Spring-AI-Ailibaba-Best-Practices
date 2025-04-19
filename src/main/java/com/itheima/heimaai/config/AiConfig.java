9fb9299438842f76b6094f7e10b78b774c1bd1ef
hllqkb <androidhtml@yandex.com>
2025-04-19 21:35:01 +0800

    refactor(ai): 更新配置和依赖，添加文件存储服务

--- src/main/java/com/itheima/heimaai/config/AiConfig.java
+++ src/main/java/com/itheima/heimaai/config/AiConfig.java
@@ -1,43 +1,24 @@
 package com.itheima.heimaai.config;
 
-import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatOptions;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
-import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
-import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.document.DocumentTransformer;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
-class AiConfig {
+public class AiConfig {
 
-    private static final String DEFAULT_PROMPT = "你是一个博学的智能聊天助手，请根据用户提问回答！";
+    // 文本分割器
     @Bean
-    @Primary
-    ChatClient chatClient(ChatClient.Builder builder,ChatMemory chatMemory) {
-    return builder.defaultSystem(DEFAULT_PROMPT)
-                // 实现 Chat Memory 的 Advisor
-                // 在使用 Chat Memory 时，需要指定对话 ID，以便 Spring AI 处理上下文。
-                .defaultAdvisors(
-                        new MessageChatMemoryAdvisor(chatMemory)
-                )
-                // 实现 Logger 的 Advisor
-                .defaultAdvisors(
-                        new SimpleLoggerAdvisor()
-                )
-                // 设置 ChatClient 中 ChatModel 的 Options 参数
-                .defaultOptions(
-                        DashScopeChatOptions.builder()
-                                .withTopP(0.7)
-                                .build()
-                )
+    public DocumentTransformer documentTransformer() {
+        return new TokenTextSplitter();
+    }
+
+    @Bean
+    ChatClient chatClient(ChatClient.Builder builder) {
+        return builder.defaultSystem("你现在不是ChatGPT，我希望你是以一个全栈开发程序员来和我对话，你的名字叫做neko")
                 .build();
     }
 
-    @Bean
-    InMemoryChatMemory inMemoryChatMemory() {
-        return new InMemoryChatMemory();
-    }
 }
