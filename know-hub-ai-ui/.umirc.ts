b861eeaef44fa8f04c57691352ea0be25c9c3275
hllqkb <androidhtml@yandex.com>
2025-04-22 21:13:38 +0800

    上传前端

---
+++ know-hub-ai-ui/.umirc.ts
@@ -0,0 +1,79 @@
+import { defineConfig } from '@umijs/max';
+
+export default defineConfig({
+  plugins: ['@umijs/max-plugin-openapi'],
+  antd: {
+    dark: true,
+  },
+  access: {},
+  model: {},
+  initialState: {},
+  request: {
+    dataField: 'data',
+  },
+  layout: {},
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: '@/pages/Login',
+      title: '登录',
+      layout: false,
+    },
+    {
+      path: '/',
+      redirect: '/chat',
+    },
+    // {
+    //   path: '/home',
+    //   name: '首页',
+    //   title: '首页',
+    //   component: '@/pages/Home',
+    //   icon: 'HomeOutlined',
+    // },
+    {
+      path: '/chat',
+      name: 'AI对话',
+      title: 'AI对话',
+      component: '@/pages/Chat',
+      icon: 'RobotOutlined',
+    },
+    {
+      path: '/knowlegeBase',
+      name: '知识库',
+      title: '知识库',
+      component: '@/pages/KnowledgeBase',
+      icon: 'BookOutlined',
+    },
+    {
+      path: '/knowlegeBase/:knowledgeBaseId',
+      name: '知识库详情',
+      title: '知识库详情',
+      component: '@/pages/Document',
+      hideInMenu: true,
+    },
+
+    { path: '/*', component: '@/pages/404' },
+  ],
+  npmClient: 'pnpm',
+  proxy: {
+    '/api': {
+      target: process.env.UMI_APP_BASE_URL || 'http://localhost:8788/api',
+      changeOrigin: true,
+      secure: false,
+      pathRewrite: {
+        '^/api': '',
+      },
+    },
+  },
+  openAPI: [
+    {
+      requestLibPath: "import { request } from '@umijs/max'",
+      schemaPath: `${process.env.UMI_APP_BASE_URL}/v3/api-docs/default`, // openapi 接口地址
+      mock: false,
+      apiPrefix() {
+        return "'/api'";
+      },
+    },
+  ],
+});
